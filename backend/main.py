ade10f64e0b3335595e30473235f8d303214615d
sinan-prvt <mohamedsinan9400@gmail.com>
2026-02-11 10:38:49 +0530

    AG-28 : Add SQLAlchemy connection and User model

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database.connection import engine, Base
+from database.models.user import User
 
 app = FastAPI(
     title="Resume Agent API",
@@ -14,6 +16,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
